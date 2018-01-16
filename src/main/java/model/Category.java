890dc5829f2a649da92f6e804ba7ee996dad571a
Sami <elyadari.sami@gmail.com>
2018-01-16 14:43:24 +0100

    Add Model Classes ( Activity , Category , Post)

---
+++ src/main/java/model/Category.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Samy
+ */
+public class Category {
+
+    private int idC ;
+    private String name ;
+
+    public Category(int idC, String name) {
+        this.idC = idC;
+        this.name = name;
+    }
+    public int getIdC() {
+        return idC;
+    }
+
+    public void setIdC(int idC) {
+        this.idC = idC;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    
+    
+}
