406984beda6b75d2308a692c549d8916e0ef3d28
Florian ARNOULD <florian.arnould@ecole.ensicaen.fr>
2018-01-18 09:10:42 +0100

    add log post gps

--- src/main/java/fr/bul/backend/posts/SearchPostService.java
+++ src/main/java/fr/bul/backend/posts/SearchPostService.java
@@ -8,6 +8,8 @@ import fr.bul.backend.model.Post;
 import fr.bul.backend.util.Utils;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -17,6 +19,7 @@ import java.util.List;
 
 public class SearchPostService implements Route {
     public static final String LOCATION = "/searchPost";
+    private static final Logger LOGGER = LoggerFactory.getLogger(SearchPostService.class);
 
     @Override
     public Object handle(Request request, Response response) {
@@ -29,6 +32,8 @@ public class SearchPostService implements Route {
             List<Post> posts = dao.getPosts(json.getString("search"));
             List<ElementToSend> elementsToSend = new ArrayList<>();
             for (Post post : posts) {
+                LOGGER.info("gps borne : " + gps.getLatitude() + " " + gps.getLongitude());
+                LOGGER.info("gps post : " + post.getCoordinates().getLatitude() + " " + post.getCoordinates().getLongitude());
                 elementsToSend.add(new ElementToSend(post, (int) Utils.distance(gps, post.getCoordinates())));
             }
             elementsToSend.sort((p1, p2) -> (p1.getDistance() - p2.getDistance()));
