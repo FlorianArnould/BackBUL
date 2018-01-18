9ddb847c0a9a464905b0c7604c1685484ee7a376
Florian ARNOULD <florian.arnould@ecole.ensicaen.fr>
2018-01-18 15:13:24 +0100

    add get

--- src/main/java/fr/bul/backend/Application.java
+++ src/main/java/fr/bul/backend/Application.java
@@ -4,6 +4,9 @@ import fr.bul.backend.activities.ActivitiesService;
 import fr.bul.backend.emergency.EmergencyService;
 import fr.bul.backend.posts.AddPostsService;
 import fr.bul.backend.posts.SearchPostService;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
 import static spark.Spark.*;
 
@@ -12,9 +15,12 @@ public class Application {
         port(8080);
         staticFileLocation("/public");
         post(ActivitiesService.LOCATION, "*/*", new ActivitiesService());
+        get(ActivitiesService.LOCATION, (request, response) -> "");
         post(AddPostsService.LOCATION, "*/*", new AddPostsService());
+        get(AddPostsService.LOCATION, (request, response) -> "");
         post(SearchPostService.LOCATION, "*/*", new SearchPostService());
+        get(SearchPostService.LOCATION, (request, response) -> "");
         post(EmergencyService.LOCATION, "*/*", new EmergencyService());
-
+        get(EmergencyService.LOCATION, (request, response) -> "");
     }
 }
